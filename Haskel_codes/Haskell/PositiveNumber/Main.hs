e133c8372fdf72858d265420beed35f22c87d411
patzer-adi <nkadityagowari@gmail.com>
2026-05-12 18:00:33 +0530

    Haskel code file

---
+++ Haskel_codes/Haskell/PositiveNumber/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+printOnedigitNum n = 
+			if n < 10
+		        then print n;
+		        else print "Not a single digit number";
+main :: IO()			
+main = do
+	putStrLn "Enter a positive integer"
+	line <- getLine
+	let number = (read line :: Int)
+	printOnedigitNum number	
